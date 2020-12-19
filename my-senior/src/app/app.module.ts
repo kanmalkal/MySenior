457699fb5e7d74963929da28b3897241af0eee43
kanmalkal <kanmalkal@gmail.com>
2020-12-19 13:13:20 -0600

    commit changes inside VS Code using Github extension

--- my-senior/src/app/app.module.ts
+++ my-senior/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonBlockComponent } from './button-block/button-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ButtonBlockComponent
   ],
   imports: [
     BrowserModule,
